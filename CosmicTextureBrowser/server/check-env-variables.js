3531294a257a5409ce3e8f790e52782cd0f70c71
Sergiu_Bo <bocanceaserge335566@gmail.com>
2024-11-19 14:49:15 +0100

    :bug: Fix vite env server IP

--- CosmicTextureBrowser/server/check-env-variables.js
+++ CosmicTextureBrowser/server/check-env-variables.js
@@ -1,9 +1,48 @@
 import fs from 'fs';
 import * as dotenv from 'dotenv';
+import { networkInterfaces } from 'os';
 
 dotenv.config();
 
 writeTextureBrowserPort();
+
+
+/**
+ * Write Server IP
+ */
+const devServerIp = getDevServerIp();
+writeDevServerIP();
+
+function getDevServerIp() {
+    const networks = networkInterfaces();
+    const devServerIp = networks.en0.filter((item) => {
+        return item.family === 'IPv4';
+    })[0].address;
+    return devServerIp;
+}
+
+function writeDevServerIP() {
+    const envFileContent = fs.readFileSync('./.env', 'utf8');
+    const envFileLines = envFileContent.split('\n');
+    let devServerLineFound = false;
+    for (let i = 0; i < envFileLines.length; i++) {
+        if (envFileLines[i].includes('VITE_DEV_SERVER_IP')) {
+            console.log(`\n🫡  Updating VITE_DEV_SERVER_IP env variable to ${devServerIp} 🫡\n`);
+            envFileLines[i] = `VITE_DEV_SERVER_IP=${devServerIp}`;
+            devServerLineFound = true;
+        }
+    }
+    if (devServerLineFound) {
+        const newEnvFileContent = envFileLines.join('\n');
+        fs.writeFileSync('./.env', newEnvFileContent, 'utf-8');
+    } else {
+        console.log('\n❌ VITE_DEV_SERVER_IP env variable was not found! ❌\n');
+        console.log('\n🫡  Creating VITE_DEV_SERVER_IP env variable 🫡\n');
+        envFileLines.push(`VITE_DEV_SERVER_IP=${devServerIp}`);
+        const newEnvFileContent = envFileLines.join('\n');
+        fs.writeFileSync('./.env', newEnvFileContent, 'utf-8');
+    }
+}
 
 function writeTextureBrowserPort() {
     const envFileContent = fs.readFileSync('./.env', 'utf8');
